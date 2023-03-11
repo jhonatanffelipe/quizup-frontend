4737e5bb785259fe66ef39f386a826a02bdd711e
Jhonatan Nascimento <jhonatanfnm@gmail.com>
2023-03-11 15:46:31 -0300

    Desenvolvendo tabela na tela de usuário

--- src/pages/userSettings/index.jsx
+++ src/pages/userSettings/index.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import {
+  FiChevronDown,
   FiChevronLeft,
   FiChevronRight,
   FiChevronsLeft,
@@ -16,9 +18,12 @@ import {
   TableBodyData,
   TableFooter,
   TableContainer,
+  PerPage,
 } from './styles'
 
 const UserSettings = () => {
+  const [rowsPerPage, serRowsPerGage] = useState(5)
+
   return (
     <Container>
       <h1>Gestão de usuários</h1>
@@ -27,51 +32,91 @@ const UserSettings = () => {
         <TableContent>
           <TableHead>
             <TableHeadRow>
-              <TableTitle>ID</TableTitle>
               <TableTitle>Nome</TableTitle>
               <TableTitle>E-mail</TableTitle>
               <TableTitle>Ativo</TableTitle>
-              <TableTitle>Administrador</TableTitle>
+              <TableTitle>Admin</TableTitle>
               <TableTitle>Criar em</TableTitle>
               <TableTitle>Atualizado em</TableTitle>
             </TableHeadRow>
           </TableHead>
           <TableBody>
             <TableBodyRow>
-              <TableBodyData>1</TableBodyData>
               <TableBodyData>Jhonatan</TableBodyData>
               <TableBodyData>jhonatanfnm@gmail.com</TableBodyData>
-              <TableBodyData>true</TableBodyData>
+              <TableBodyData>
+                <input
+                  type="checkbox"
+                  disabled
+                  checked={false}
+                  name="active"
+                  id="active"
+                />
+              </TableBodyData>
+              <TableBodyData>
+                <input
+                  type="checkbox"
+                  disabled
+                  checked={true}
+                  name="admin"
+                  id="admin"
+                />
+              </TableBodyData>
               <TableBodyData>10/03/2023 08:00:00</TableBodyData>
               <TableBodyData>10/03/2023 08:00:00</TableBodyData>
             </TableBodyRow>
             <TableBodyRow>
-              <TableBodyData>1</TableBodyData>
               <TableBodyData>Jhonatan</TableBodyData>
               <TableBodyData>jhonatanfnm@gmail.com</TableBodyData>
-              <TableBodyData>true</TableBodyData>
+              <TableBodyData>
+                <input
+                  type="checkbox"
+                  disabled
+                  checked={true}
+                  name="active"
+                  id="active"
+                />
+              </TableBodyData>
+              <TableBodyData>
+                <input
+                  type="checkbox"
+                  disabled
+                  checked={true}
+                  name="admin"
+                  id="admin"
+                />
+              </TableBodyData>
               <TableBodyData>10/03/2023 08:00:00</TableBodyData>
               <TableBodyData>10/03/2023 08:00:00</TableBodyData>
             </TableBodyRow>
           </TableBody>
         </TableContent>
+        <TableFooter>
+          <PerPage>
+            <span>Linhas por página:</span>
+            <div>
+              {rowsPerPage}
+              <FiChevronDown />
+            </div>
+          </PerPage>
+
+          <button>
+            <FiChevronsLeft />
+          </button>
+          <button>
+            <FiChevronLeft />
+          </button>
+
+          <span>1 de 3</span>
+
+          <button>
+            <FiChevronRight />
+          </button>
+          <button>
+            <FiChevronsRight />
+          </button>
+        </TableFooter>
       </TableContainer>
-      <TableFooter>
-        <div>2 de 25</div>
-        <button>
-          <FiChevronsLeft />
-        </button>
-        <button>
-          <FiChevronLeft />
-        </button>
-        <div>1 de 3</div>
-        <button>
-          <FiChevronRight />
-        </button>
-        <button>
-          <FiChevronsRight />
-        </button>
-      </TableFooter>
     </Container>
   )
 }
--- src/pages/userSettings/styles.js
+++ src/pages/userSettings/styles.js
@@ -11,22 +11,97 @@ export const Container = styled.div`
 export const TableContainer = styled.div`
   display: flex;
   flex-direction: column;
+  background: #151922;
+  padding: 16px;
+  border-radius: 8px;
+  margin-top: 16px;
+  font-size: 14px;
 `
 
-export const TableContent = styled.table``
+export const TableContent = styled.table`
+  border-collapse: collapse;
+`
 
 export const TableHead = styled.thead``
 
-export const TableHeadRow = styled.tr``
+export const TableHeadRow = styled.tr`
+  height: 48px;
+`
 
-export const TableTitle = styled.th``
+export const TableTitle = styled.th`
+  text-align: start;
+  padding: 0 8px;
+`
 
 export const TableBody = styled.tbody``
 
-export const TableBodyRow = styled.tr``
+export const TableBodyRow = styled.tr`
+  height: 36px;
+  cursor: pointer;
+  &:hover {
+    background: #2a2d33;
+  }
+  border-radius: 8px;
 
-export const TableBodyData = styled.td``
+  input {
+    cursor: pointer;
+  }
+`
+
+export const TableBodyData = styled.td`
+  padding: 0 8px;
+  border-collapse: collapse;
+`
 
 export const TableFooter = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 12px;
+  font-size: 12px;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background: transparent;
+    color: #fff;
+    padding: 6px;
+    border-radius: 4px;
+
+    &:hover {
+      background: #2a2d33;
+    }
+  }
+
+  > span {
+    margin: 4px;
+  }
+`
+export const PerPage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-left: 8px;
+    margin-right: 16px;
+    width: 60px;
+    border-radius: 4px;
+    padding: 6px;
+
+    svg {
+      margin-left: 8px;
+    }
+
+    cursor: pointer;
+
+    &:hover {
+      background: #2a2d33;
+    }
+  }
 `
