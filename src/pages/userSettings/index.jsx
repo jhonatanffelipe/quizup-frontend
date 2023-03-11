0da09979954c5229bc077e53d5f371556a403f60
Jhonatan Nascimento <jhonatanfnm@gmail.com>
2023-03-11 11:18:53 -0300

    Desenvolvendo tabela na tela de usuário

--- src/pages/userSettings/index.jsx
+++ src/pages/userSettings/index.jsx
@@ -1,9 +1,77 @@
-import { Container } from './styles'
+import {
+  FiChevronLeft,
+  FiChevronRight,
+  FiChevronsLeft,
+  FiChevronsRight,
+} from 'react-icons/fi'
+
+import {
+  Container,
+  TableContent,
+  TableHead,
+  TableHeadRow,
+  TableTitle,
+  TableBody,
+  TableBodyRow,
+  TableBodyData,
+  TableFooter,
+  TableContainer,
+} from './styles'
 
 const UserSettings = () => {
   return (
     <Container>
       <h1>Gestão de usuários</h1>
+
+      <TableContainer>
+        <TableContent>
+          <TableHead>
+            <TableHeadRow>
+              <TableTitle>ID</TableTitle>
+              <TableTitle>Nome</TableTitle>
+              <TableTitle>E-mail</TableTitle>
+              <TableTitle>Ativo</TableTitle>
+              <TableTitle>Administrador</TableTitle>
+              <TableTitle>Criar em</TableTitle>
+              <TableTitle>Atualizado em</TableTitle>
+            </TableHeadRow>
+          </TableHead>
+          <TableBody>
+            <TableBodyRow>
+              <TableBodyData>1</TableBodyData>
+              <TableBodyData>Jhonatan</TableBodyData>
+              <TableBodyData>jhonatanfnm@gmail.com</TableBodyData>
+              <TableBodyData>true</TableBodyData>
+              <TableBodyData>10/03/2023 08:00:00</TableBodyData>
+              <TableBodyData>10/03/2023 08:00:00</TableBodyData>
+            </TableBodyRow>
+            <TableBodyRow>
+              <TableBodyData>1</TableBodyData>
+              <TableBodyData>Jhonatan</TableBodyData>
+              <TableBodyData>jhonatanfnm@gmail.com</TableBodyData>
+              <TableBodyData>true</TableBodyData>
+              <TableBodyData>10/03/2023 08:00:00</TableBodyData>
+              <TableBodyData>10/03/2023 08:00:00</TableBodyData>
+            </TableBodyRow>
+          </TableBody>
+        </TableContent>
+      </TableContainer>
+      <TableFooter>
+        <div>2 de 25</div>
+        <button>
+          <FiChevronsLeft />
+        </button>
+        <button>
+          <FiChevronLeft />
+        </button>
+        <div>1 de 3</div>
+        <button>
+          <FiChevronRight />
+        </button>
+        <button>
+          <FiChevronsRight />
+        </button>
+      </TableFooter>
     </Container>
   )
 }
--- src/pages/userSettings/styles.js
+++ src/pages/userSettings/styles.js
@@ -5,4 +5,28 @@ export const Container = styled.div`
   padding: 15px;
   background: #2a2d33;
   height: 100vh;
+  padding-top: 24px;
+`
+
+export const TableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const TableContent = styled.table``
+
+export const TableHead = styled.thead``
+
+export const TableHeadRow = styled.tr``
+
+export const TableTitle = styled.th``
+
+export const TableBody = styled.tbody``
+
+export const TableBodyRow = styled.tr``
+
+export const TableBodyData = styled.td``
+
+export const TableFooter = styled.div`
+  display: flex;
 `
